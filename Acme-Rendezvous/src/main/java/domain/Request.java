e163b6a027140e259dd450db8a4801d961f640df
danirguez <32953186+danirguez@users.noreply.github.com>
2018-03-01 11:54:14 +0100

    feat: conceptual model

--- Acme-Rendezvous/src/main/java/domain/Request.java
+++ Acme-Rendezvous/src/main/java/domain/Request.java
@@ -4,6 +4,7 @@ import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Access(AccessType.PROPERTY)
@@ -15,6 +16,7 @@ public class Request extends DomainEntity  {
 	private String comment;
 
 	@Valid
+	@NotNull
 	public CreditCard getCreditCard() {
 		return creditCard;
 	}
