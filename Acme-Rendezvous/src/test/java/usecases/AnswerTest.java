1c49450160e8557b1c0319102c8e375a6f20ca48
danirguez <32953186+danirguez@users.noreply.github.com>
2018-03-23 00:00:06 +0100

    feea: test answer

--- Acme-Rendezvous/src/test/java/usecases/AnswerTest.java
+++ Acme-Rendezvous/src/test/java/usecases/AnswerTest.java
@@ -36,16 +36,26 @@ public class AnswerTest extends AbstractTest {
 		final Object testingData[][] = {
 
 			{
+				// Answer the questions that are associated with a rendezvous that he or she RSVPing now.
 				"user1", "an answer", "question1", null
 			}, {
 				"user2", "another answer", "question1", IllegalArgumentException.class
 			}, {
 				null, "another more answer", "question1", IllegalArgumentException.class
+			} , {
+				// Edit one of his answer
+				"user1", "answer1", null
+			}, {
+				// Edit another answer
+				"user1", "answer2", IllegalArgumentException.class
 			}
 		};
 
-		for (int i = 0; i < testingData.length; i++)
+		for (int i = 0; i < 3; i++)
 			this.createAnswerTemplate((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (Class<?>) testingData[i][3]);
+		
+		for (int i = 3; i < testingData.length; i++)
+			this.editAnswerTemplate((String) testingData[i][0], (String) testingData[i][1], (Class<?>) testingData[i][2]);
 
 	}
 	protected void createAnswerTemplate(final String user, final String text, final String question, final Class<?> expected) {
@@ -69,5 +79,23 @@ public class AnswerTest extends AbstractTest {
 			caught = oops.getClass();
 		}
 		this.checkExceptions(expected, caught);
+	}
+	
+	private void editAnswerTemplate(final String user, final String answery, final Class<?> expected) {
+		Class<?> caught;
+		caught = null;
+		try {
+			super.authenticate(user);
+			int answerId = this.getEntityId(answery);
+			Answer answer = this.answerService.findOne(answerId);
+			answer.setText("something");
+			this.answerService.save(answer);
+			this.unauthenticate();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.checkExceptions(expected, caught);
+
 	}
 }
